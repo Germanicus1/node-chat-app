b7406d6dcc507ba627178c5f612fbdbc24b022cd
Peter Kerschbaumer <peter@kerschbaumer.se>
2019-01-21 13:09:34 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 let socket = io();
 socket.on('connect', () => {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'carmen',
-    text: 'Hola! Esto es un test.'
-  });
 });
 
 socket.on('disconnect', () => {
--- server/server.js
+++ server/server.js
@@ -14,16 +14,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.on('createMessage', (newMessage) => {
-    console.log('createMessage', newMessage);
-  })
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
 
-  socket.emit('newMessage', {
-    from: 'Max',
-    text: 'Hej,detta är ett test',
-    createdAt: 123
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
-  
 
   socket.on('disconnect', () => {
     console.log('Client disconnected');
