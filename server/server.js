d4030755b5d3dfd3416ff92c958288c2edbca5c2
Peter Kerschbaumer <peter@kerschbaumer.se>
2019-01-21 12:12:12 +0100

    Add new message and create message events

--- server/server.js
+++ server/server.js
@@ -12,8 +12,19 @@ const io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('New user connected')
+  console.log('New user connected');
+
+  socket.on('createMessage', (newMessage) => {
+    console.log('createMessage', newMessage);
+  })
+
+  socket.emit('newMessage', {
+    from: 'Max',
+    text: 'Hej,detta är ett test',
+    createdAt: 123
+  });
   
+
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
